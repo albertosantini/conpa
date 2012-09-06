7c1e967423749e382d594dc086bb1c48398a68a2
icebox <albertosantini@gmail.com>
2012-09-06 16:26:00 +0200

    Update jQuery 1.8.1.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
             vendor: {
                 src: [
                     'lib/public/ConPA/assets/js/json2-20111019.js',
-                    'lib/public/ConPA/assets/js/jquery-1.8.0.min.js',
+                    'lib/public/ConPA/assets/js/jquery-1.8.1.min.js',
                     'lib/public/ConPA/assets/js/jquery.pubsub-0.7.min.js',
                     'lib/public/ConPA/assets/js/jquery.sparkline-2.0.min.js',
                     'lib/public/ConPA/assets/js/jquery.localstorage-0.1.js',

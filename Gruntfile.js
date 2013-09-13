7c4a55850d3343182e6e14682469e5d39330def6
icebox <albertosantini@gmail.com>
2013-09-13 20:53:23 +0200

    Update Lo-Dash 2.0.0.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
                     'lib/public/ConPA/assets/js/jquery.pubsub-0.7.min.js',
                     'lib/public/ConPA/assets/js/jquery.sparkline-2.1.2.min.js',
                     'lib/public/ConPA/assets/js/jquery.localstorage-0.1.js',
-                    'lib/public/ConPA/assets/js/lodash-1.3.1.min.js',
+                    'lib/public/ConPA/assets/js/lodash-2.0.0.min.js',
                     'lib/public/ConPA/assets/js/typeahead-2.3.2.min.js'
                 ],
                 dest: 'lib/public/ConPA/build/vendor.js'

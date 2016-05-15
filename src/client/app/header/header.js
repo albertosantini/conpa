973154bca37dcf537896fe31f43b1d0bf0221620
icebox <albertosantini@gmail.com>
2016-05-15 20:23:22 +0200

    Fix progress in header.

--- src/client/app/header/header.js
+++ src/client/app/header/header.js
@@ -4,6 +4,17 @@
     angular
         .module("conpa")
         .component("header", {
+            controller: Header,
             templateUrl: "app/header/header.html"
         });
+
+    Header.$inject = ["$rootScope"];
+    function Header($rootScope) {
+        var vm = this;
+
+        $rootScope.$watch("isLoadingView", function () {
+            vm.isLoadingView = $rootScope.isLoadingView;
+        });
+    }
+
 }());

1a6837addd648a5684b854b5f8b1b1acda871081
icebox <albertosantini@gmail.com>
2018-06-18 18:55:45 +0200

    Use a table for the basket.

--- src/client/app/components/basket/basket.template.js
+++ src/client/app/components/basket/basket.template.js
@@ -1,15 +1,33 @@
 export class BasketTemplate {
     static update(render, state, events) {
+        const headerClasses = "fw6 bb b--black-20 tl pb1 pr1 bg-white tr";
+        const trClasses = "pv1 pr1 bb b--black-20 tr";
+
         /* eslint-disable indent */
         render`
             <h2>Basket</h2>
 
             <input id="assetsSearch" size="32" oninput="${events}" placeholder="What assets to be added?">
-            <ul onclick="${events}">${state.assetsSearch.map(asset =>
-                `<li id="assetSearch-${asset.symbol}" data-value='${escape(JSON.stringify(asset))}' class="pointer dim">
-                    ${asset.symbol} ${asset.name} (${asset.type} - ${asset.exchDisp})
-                </li>`)
-            }</ul>
+            <table class="f7 mw8 pa2" cellpsacing="0">
+                <thead>
+                    <th class="${headerClasses}">Symbol</th>
+                    <th class="${headerClasses}">Description</th>
+                    <th class="${headerClasses}">Type</th>
+                    <th class="${headerClasses}">Market</th>
+                </thead>
+
+                <tbody onclick="${events}" >${state.assetsSearch.map(asset =>
+                    `<tr>
+                        <td id="assetSearch-${asset.symbol}"
+                            class="${trClasses} pointer dim"
+                            data-value='${escape(JSON.stringify(asset))}'
+                            title="Click to add the asset">${asset.symbol}</td>
+                        <td class="${trClasses}">${asset.name}</td>
+                        <td class="${trClasses}">${asset.type}</td>
+                        <td class="${trClasses}">${asset.exchDisp}</td>
+                    </tr>`)
+                }</tbody>
+            </table>
         `;
         /* eslint-enable indent */
     }

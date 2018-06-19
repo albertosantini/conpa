4d7729e39cb795cd676fd21940ceb46f2f983f6f
icebox <albertosantini@gmail.com>
2018-06-19 18:04:37 +0200

    Measure server roundtrip.

--- src/client/app/components/header/header.template.js
+++ src/client/app/components/header/header.template.js
@@ -2,6 +2,8 @@ import { HeaderService } from "./header.service.js";
 
 export class HeaderTemplate {
     static update(render) {
+        const start = Date.now();
+
         /* eslint-disable indent */
         render`
             <nav class="flex flex-row bt bb tc mw9 center shadow-2">
@@ -16,7 +18,14 @@ export class HeaderTemplate {
                 <div class="flex flex-wrap flex-row items-center min-w-30>
                     <span class="f7">
                         <toasts></toasts>
-                        <div>${{ any: HeaderService.getStatus().then(({ namespace: status }) => status.message), placeholder: "Loading..." }}</div>
+                        <div>${{
+                            any: HeaderService.getStatus().then(({ namespace: status }) => {
+                                const end = Date.now();
+
+                                return `${status.message} (${end - start}ms)`;
+                            }),
+                            placeholder: "Loading..."
+                        }}</div>
                     </span>
                 </div>
 

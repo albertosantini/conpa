a6cb4693fde68ab8cfabeadd867c943cdbb852d5
icebox <albertosantini@gmail.com>
2018-07-20 09:16:35 +0200

    Fix getYYYYMMDDfromDate with the day of the month.

--- src/client/app/util.js
+++ src/client/app/util.js
@@ -22,7 +22,7 @@ export class Util {
 
         const year = date.getFullYear();
         const month = (date.getMonth() + 1).toString().padStart(2, "0");
-        const day = date.getDay().toString().padStart(2, "0");
+        const day = date.getDate().toString().padStart(2, "0");
 
         return `${year}/${month}/${day}`;
     }

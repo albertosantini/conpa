de54a6ea7838a5fcfae80b99c895b483c856ee34
Alberto Santini <albertosantini@pm.me>
2020-02-15 16:15:01 +0100

    Ass logo image

--- src/client/app/components/header/header.template.js
+++ src/client/app/components/header/header.template.js
@@ -5,6 +5,7 @@ export class HeaderTemplate {
             <nav class="flex flex-row bt bb mh5 shadow-2">
 
                 <div class="flex flex-wrap flex-row justify-around items-center min-w-70 b logo">
+                    <img class="logo" src="/img/bleeding-edge.png">
                     <a href="https://github.com/albertosantini/node-conpa">ConPA 6</a>
                     <span>Asset Allocation App</span>
                 </div>

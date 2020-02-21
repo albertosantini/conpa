0d2610b4ffa2f55afe538801515fca39d31af903
Alberto Santini <albertosantini@pm.me>
2020-02-21 18:17:02 +0100

    Remove api folder in src

--- scripts/build.js
+++ scripts/build.js
@@ -5,10 +5,8 @@ const path = require("path");
 const shell = require("shelljs");
 
 const outputFolder = path.join(__dirname, "../public/");
-const apiFolder = path.join(__dirname, "../api/");
 const outputNodeModulesFolder = path.join(__dirname, "../public/node_modules");
 const sourceFolder = path.join(__dirname, "../src/client/*");
-const apiSourceFolder = path.join(__dirname, "../src/api/*");
 const nodeModulesFolder = path.join(__dirname, "../node_modules");
 const hyperhtmlDep = path.join(nodeModulesFolder, "hyperhtml/");
 const tachyonslDep = path.join(nodeModulesFolder, "tachyons/");
@@ -18,15 +16,10 @@ if (!fs.existsSync(outputFolder)) {
     fs.mkdirSync(outputFolder);
 }
 
-if (!fs.existsSync(apiFolder)) {
-    fs.mkdirSync(apiFolder);
-}
-
 if (!fs.existsSync(outputNodeModulesFolder)) {
     fs.mkdirSync(outputNodeModulesFolder);
 }
 
 shell.cp("-R", sourceFolder, outputFolder);
-shell.cp("-R", apiSourceFolder, apiFolder);
 shell.cp("-R", hyperhtmlDep, `${outputNodeModulesFolder}/hyperhtml/`);
 shell.cp("-R", tachyonslDep, `${outputNodeModulesFolder}/tachyons/`);

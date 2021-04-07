7ed6166659c6d59367cc580445bfb3b3dafd5c87
Alberto Santini <albertosantini@pm.me>
2021-04-07 12:12:30 +0200

    Suppress TS errors for skypack modules

---
+++ skypack.d.ts
@@ -0,0 +1,4 @@
+// https://docs.skypack.dev/skypack-cdn/code/javascript#using-skypack-urls-in-typescript
+// First, let TypeScript allow all module names starting with "https://".
+// This will suppress TS errors.
+declare module "https://*";
